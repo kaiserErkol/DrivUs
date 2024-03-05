e29a1cdbff997ec46847bda80728f1a312798d42
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-03-05 21:13:35 +0100

    added LocationManager

--- DrivUs/DrivUs/ContentView.swift
+++ DrivUs/DrivUs/ContentView.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let location = locationManager.location {
+                            Text("Your location: \(location.latitude), \(location.longitude)")
+                        }
+            LocationButton {
+                locationManager.requestLocation()
+            }
+            .frame(height: 44)
         }
         .padding()
     }
