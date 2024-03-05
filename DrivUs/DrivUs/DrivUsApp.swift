d21c4deda31a635dc784ad900f0246d0541d328b
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-03-05 09:15:59 +0100

    created Project

---
+++ DrivUs/DrivUs/DrivUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivUsApp.swift
+//  DrivUs
+//
+//  Created by MacBook on 05.03.24.
+//
+
+import SwiftUI
+
+@main
+struct DrivUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
