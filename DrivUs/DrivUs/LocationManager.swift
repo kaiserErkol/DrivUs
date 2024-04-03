f850a5a17e91a55e3364b5ad7c036f73230a0aa1
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-04-03 08:10:35 +0200

    Distanz Berechnung

--- DrivUs/DrivUs/LocationManager.swift
+++ DrivUs/DrivUs/LocationManager.swift
@@ -26,9 +26,40 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         location = locations.first?.coordinate
         print(location.self as Any)
+        
+        // Example calculation of Haversine distance
+        let lat1 = 51.5007
+        let lon1 = 0.1246
+        let lat2 = location?.latitude ?? 0.0
+        let lon2 = location?.longitude ?? 0.0
+        let distance = haversine(lat1: lat1, lon1: lon1, lat2: lat2, lon2: lon2)
+        print("Distance from fixed location: \(distance) km")
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Location manager failed with error: \(error.localizedDescription)")
+    }
+    
+    func haversine(lat1: Double, lon1: Double, lat2: Double, lon2: Double) -> Double {
+        // distance between latitudes and longitudes
+        let dLat = lat2.toRadians - lat1.toRadians
+        let dLon = lon2.toRadians - lon1.toRadians
+        
+        // convert to radians
+        let radLat1 = lat1.toRadians
+        let radLat2 = lat2.toRadians
+        
+        // apply formulae
+        let a = sin(dLat / 2) * sin(dLat / 2) +
+            sin(dLon / 2) * sin(dLon / 2) * cos(radLat1) * cos(radLat2)
+        let rad = 6371.0 // Earth radius in kilometers
+        let c = 2 * asin(sqrt(a))
+        return rad * c
+    }
+}
+
+extension Double {
+    var toRadians: Double {
+        return self * .pi / 180.0
     }
 }
