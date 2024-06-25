d0e2a295e6ea32a3b67abc224fede4f4d98f2cd6
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-06-25 13:52:42 +0200

    detail view

---
+++ DrivUs/DrivUs/View/PersonDetailView.swift
@@ -0,0 +1,62 @@
+//
+//  PersonDetailView.swift
+//  DrivUs
+//
+//  Created by MacBook on 25.06.24.
+//
+
+import SwiftUI
+
+struct PersonDetailView: View {
+    @ObservedObject var viewModelRides: ViewModel_Rides
+
+    var match: Model.MatchModel.Match
+
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                if let rideByMatch = viewModelRides.fetchRideById(match.rideId) {
+                    MapView(
+                        sp_latitude: rideByMatch.startpunkt_latitude,
+                        sp_longitude: rideByMatch.startpunkt_longitude,
+                        ep_latitude: rideByMatch.endpunkt_latitude,
+                        ep_longitude: rideByMatch.endpunkt_longitude,
+                        startName: rideByMatch.startpunkt_ort,
+                        endName: rideByMatch.endpunkt_ort
+                    )
+                    .frame(height: 400)
+                    
+                    VStack(spacing: 10) {
+                        VStack {
+                            Text("\(rideByMatch.startpunkt_ort) - \(rideByMatch.endpunkt_ort)")
+                                .padding(.top, 20)
+                                .foregroundColor(.black)
+                                .kerning(7)
+                        }
+                        .frame(width: UIScreen.main.bounds.width)
+                        .padding(.bottom, 10)
+
+                        Button(action: {
+                            // Button action here
+                        }) {
+                            Text("Anfrage senden")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+
+                    }
+                    .padding(.top, 20)
+                    .padding([.leading, .trailing], 16)
+                } else {
+                    Text("No ride found")
+                        .padding()
+                        .foregroundColor(.black)
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            .cornerRadius(20)
+        }
+    }
+}
--- DrivUs/DrivUs/View/PersonView.swift
+++ DrivUs/DrivUs/View/PersonView.swift
@@ -16,36 +16,39 @@ struct PersonView: View {
         VStack {
             let loggedUserId = viewModel_user.loggedUser.id
             
-            NavigationView {
+            NavigationStack {
                 List(viewModel_matches.matches) { match in
                     let userMatchId = (match.firstUserId != loggedUserId) ? match.firstUserId : match.secondUserId
                     
                     let userById = viewModel_user.fetchUserById(userMatchId)
 
-                    HStack{
-                        Image("\(userById?.name ?? "Unknown")")
-                            .resizable()
-                            .frame(width: 70, height: 70)
-                            .background(Color.drivusBlue)
-                            .foregroundColor(.white)
-                            .clipShape(Circle())
-                            .padding(.top,2)
-                            .padding(.bottom,2)
-                        
-                        VStack(alignment: .leading) {
-                            Text(" \(userById?.name ?? "Unknown")")
+                    NavigationLink(value: match) {
+                        HStack{
+                            Image("\(userById?.name ?? "Unknown")")
+                                .resizable()
+                                .frame(width: 70, height: 70)
+                                .background(Color.drivusBlue)
                                 .foregroundColor(.white)
-                                .font(.title)
-                            Text("   \(viewModel_rides.getRideById(match.rideId)?.startpunkt_ort ?? "Unknown") - \(viewModel_rides.getRideById(match.rideId)?.endpunkt_ort ?? "Unknown")")
-                                .font(.body)
-                                .foregroundColor(.black)
+                                .clipShape(Circle())
+                                .padding(.top,2)
+                                .padding(.bottom,2)
+                            
+                            VStack(alignment: .leading) {
+                                Text(" \(userById?.name ?? "Unknown")")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                Text("   \(viewModel_rides.getRideById(match.rideId)?.startpunkt_ort ?? "Unknown") - \(viewModel_rides.getRideById(match.rideId)?.endpunkt_ort ?? "Unknown")")
+                                    .font(.body)
+                                    .foregroundColor(.black)
+                            }
                         }
+                        
+                        .padding(10)
+                        .background(Color(UIColor.drivusBlue))
+                        .cornerRadius(10)
+                        .shadow(color: Color.darkDrivusBlue.opacity(0.1), radius: 5, x: 0, y: 2)
+                         
                     }
-                    
-                    .padding(10)
-                    .background(Color(UIColor.drivusBlue))
-                    .cornerRadius(10)
-                    .shadow(color: Color.darkDrivusBlue.opacity(0.1), radius: 5, x: 0, y: 2)
                 }
                 .navigationTitle("Meine Matches")
                 .padding(.top,10)
@@ -54,9 +57,9 @@ struct PersonView: View {
                 .task {
                     viewModel_matches.fetchUserMatches(viewModel_user.loggedUser)
                 }
-                .onAppear {
-                    
-                }
+                .navigationDestination(for: Model.MatchModel.Match.self, destination: { match in
+                    PersonDetailView(viewModelRides: viewModel_rides, match: match)
+                })
             }.navigationViewStyle(StackNavigationViewStyle())
         }
     }
